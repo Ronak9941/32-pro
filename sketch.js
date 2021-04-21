c1e37990657cc84943c4dd3e41c5fae8972ec96b
Ronak9941 <81176751+Ronak9941@users.noreply.github.com>
2021-04-21 19:12:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,117 +4,119 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
-var bird, slingshot;
+var backgroundImg;
 
-var gameState = "onSling";
-var bg = "sprites/bg1.png";
-var score = 0;
+var bg = "sunrise1.png";
+
+var time = 0;
+var HOUR;
+
+
+
 
 function preload() {
+    // create getBackgroundImg( ) here
     getBackgroundImg();
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    var canvas = createCanvas(1200,700);
     engine = Engine.create();
     world = engine.world;
 
-
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
-    if(backgroundImg)
+
+    // add condition to check if any background image is there to add
+
+    if(backgroundImg){
+
+   
         background(backgroundImg);
-    
-        noStroke();
-        textSize(35)
-        fill("white")
-        text("Score  " + score, width-300, 50)
-    
-    Engine.update(engine);
-    //strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    pig1.score();
-    log1.display();
+        }
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    pig3.score();
-    log3.display();
+        Engine.update(engine);
 
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();    
-}
-
-function mouseDragged(){
-    //if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
-}
-
-
-function mouseReleased(){
-    slingshot.fly();
-    gameState = "launched";
-}
-
-function keyPressed(){
-    if(keyCode === 32 && bird.body.speed < 1){
-       slingshot.attach(bird.body);
-       bird.trajectory = [];
-       Matter.Body.setPosition(bird.body, {x: 200 , y: 50});
-      
+        textSize(30);
+    fill("red");
+    if(HOUR<=12){
+    text("time : " + HOUR %12 + "AM",width  - 150,100)
     }
+    else{
+        text("time : " + HOUR %12 + "PM",width  - 150,100)
+        }
+   
+    // write code to display time in correct format here
+
+
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
-    var responseJSON = await response.json();
 
-    var datetime = responseJSON.datetime;
-    var hour = datetime.slice(11,13);
+   
+
+    var response  = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var ResJSON  = await response.json()
+    var DT = ResJSON.datetime;
+     HOUR = DT.slice(11,13);
+    //console.log(HOUR)
+    if(HOUR >= 06 && HOUR<= 07){
+        bg = "sunrise1.png"
+    }
+
+      if(HOUR >= 05  && HOUR <= 07){
+            bg = "sunrise2.png";
+      }
+
+
+      if(HOUR >= 07  && HOUR <= 09){
+        bg = "sunrise3.png";
+  }
+
+      if(HOUR >= 09  && HOUR <= 11){
+      bg = "sunrise4.png";
+        }
+
+        if(HOUR >= 11  && HOUR <= 13){
+            bg = "sunrise5.png";
+      }
+
+      if(HOUR >= 13  && HOUR <= 16){
+        bg = "sunrise6  .png";
+  }
+
+  if(HOUR >= 16  && HOUR <= 17){
+    bg = "sunset7.png";
+    }
+
+    if(HOUR >= 17  && HOUR <= 18){
+    bg = "sunset8.png";
+    }
+
+    if(HOUR >= 18  && HOUR <= 19){
+        bg = "sunset9.png";
+    }
+
+    if(HOUR >= 19  && HOUR <= 20){
+        bg = "sunset10.png";
+    }
     
-    if(hour>=0600 && hour<=1900){
-        bg = "sprites/bg1.png";
-    }
-    else{
-        bg = "sprites/bg2.jpg";
+    if(HOUR >= 20  && HOUR <= 24){
+        bg = "sunset11.png";
     }
 
-    backgroundImg = loadImage(bg);
-    console.log(backgroundImg);
+    if(HOUR >= 24  && HOUR <= 05){
+        bg = "sunset12.png";
+  }
+
+
+
+  
+
+
+
+
+      backgroundImg = loadImage(bg);
+
 }
